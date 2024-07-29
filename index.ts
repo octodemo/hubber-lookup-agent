1ff2dd946890028dcf78412785d80a49c238f9a0
Sebass van Boxel <hello@svboxel.com>
2024-07-29 13:10:19 +0000

    chore: Update devcontainer configuration and remove unused files

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
-import { OpenAI } from "openai";
-import type { ChatCompletionStreamParams } from "openai/lib/ChatCompletionStream.mjs";
 import { z } from "zod";
-
 const Message = z.object({
   role: z.string(),
   name: z.string().optional(),
@@ -11,8 +8,6 @@ const Message = z.object({
 const Input = z.object({
   messages: z.array(Message),
 });
-
-const openai = new OpenAI({ apiKey: Bun.env.OPENAI_API_KEY });
 
 Bun.serve({
   port: Bun.env.PORT ?? "3000",
@@ -27,7 +22,6 @@ Bun.serve({
     }
 
     // Parsing with Zod strips unknown Copilot-specific fields in the request
-    // body, which cause OpenAI errors if they're included.
     const json = await request.json();
     const input = Input.safeParse(json);
 
@@ -39,44 +33,28 @@ Bun.serve({
     console.debug("received input", JSON.stringify(json, null, 4));
     console.debug("received messages", JSON.stringify(messages, null, 4));
 
-    // Insert a special hackery system message in our message list.
-    messages.splice(-1, 0, {
-      role: "system",
-      content: "Please use entirely made-up hackery-sounding terminology.",
-    });
+    let content = null;
+    if (messages[messages.length - 1].content.includes('ping')) {
+      content = "pong";
+    } else {
+      content = "I'm sorry, I don't understand that command. Please try again.";
+    }
 
-    const openaiRequestBody = {
-      messages,
-      model: "gpt-4-1106-preview",
-      stream: true,
+    const data = {
+      "id": "copilot-no-llm",
+      "object": "chat.completion.chunk",
+      "created": (new Date()).getTime(),
+      "choices": [
+        {
+          "delta": {
+            "content": content
+          },
+        },
+      ]
     };
-
-    const stream = openai.beta.chat.completions.stream(
-      openaiRequestBody as ChatCompletionStreamParams,
-    );
-
-    console.debug(
-      "sending request to OpenAI",
-      JSON.stringify(openaiRequestBody, null, 4),
-    );
-
-    // Proxy the OpenAI API response right back to the extensibility platform.
     return new Response(
-      new ReadableStream({
-        async start(controller) {
-          stream.on("end", () => {
-            controller.close();
-          });
-
-          stream.on("chunk", (chunk) =>
-            controller.enqueue(`data: ${JSON.stringify(chunk)}\n\n`),
-          );
-        },
-
-        cancel() {
-          stream.abort();
-        },
-      }),
+      `data: ${JSON.stringify(data)}\n\n`,
+      { headers: { "Content-Type": "text/event-stream" } }
     );
   },
 });
